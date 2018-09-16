b56dab3f3820d4dd6a5bad269d999471099ab540
두히 <@DESKTOP-503K8G2>
2018-09-16 15:56:28 +0900

    예제 2-1

---
+++ HelloWorld/src/first/CharToCode.java
@@ -0,0 +1,13 @@
+package first;
+
+public class CharToCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		int code = (int) ch;
+		
+		System.out.println(ch);
+		System.out.println(code);
+	}
+}
