5d956f57c28b5757e8a1bd1256e9a95e4b4164ff
두히 <@DESKTOP-503K8G2>
2018-09-16 16:56:44 +0900

    예제2-5

---
+++ HelloWorld/src/first/ByteOverflow.java
@@ -0,0 +1,18 @@
+package first;
+
+public class ByteOverflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 0;
+		int i = 0;
+		
+		for(int x=0 ; x <= 270 ; x++)
+		{
+			System.out.println(b++);
+			System.out.println('\t');
+			System.out.println(i++);
+		}
+	}
+
+}
