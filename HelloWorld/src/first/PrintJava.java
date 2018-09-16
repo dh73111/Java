1a6043e5e1f18d46476dc323997e17fd2d32e38d
두히 <@DESKTOP-503K8G2>
2018-09-16 16:37:13 +0900

    예제 자바 출력

---
+++ HelloWorld/src/first/PrintJava.java
@@ -0,0 +1,13 @@
+package first;
+
+public class PrintJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Java";
+		String name2 = "ja" + "va";
+		
+		System.out.println(name);
+		System.out.println(name2);
+	}
+}
