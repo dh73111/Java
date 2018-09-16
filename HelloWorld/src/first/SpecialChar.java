82696f41904321830945dd8fce8e39c51a257232
두히 <@DESKTOP-503K8G2>
2018-09-16 16:03:12 +0900

    예제2-3

---
+++ HelloWorld/src/first/SpecialChar.java
@@ -0,0 +1,16 @@
+package first;
+
+public class SpecialChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char single = '\'';
+		String dblQuote = "\"Hello\"";
+		String root = "C:\\";
+		
+		System.out.println(single);
+		System.out.println(dblQuote);
+		System.out.println(root);
+	}
+
+}
